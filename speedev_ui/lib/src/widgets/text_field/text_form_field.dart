7333cdc694b48392d834ecd4fe6a9a3f6e0c9824
Emir Bolat <emirybolat@hotmail.com>
2025-06-19 00:31:37 +0300

    Enhance SDTextInput widget with additional properties and functionality
    - Added multiple customizable properties to the SDTextInput widget, including controller, hintText, labelText, keyboardType, and more.
    - Updated the build method to utilize the new properties in the PlatformTextFormField.
    - Improved input handling with onChanged, onSubmitted, and validator functions.

--- speedev_ui/lib/src/widgets/text_field/text_form_field.dart
+++ speedev_ui/lib/src/widgets/text_field/text_form_field.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter/services.dart';
 
 class SDTextInput extends StatefulWidget {
-  const SDTextInput({super.key});
+  final TextEditingController? controller;
+  final String? hintText;
+  final String? labelText;
+  final TextInputType? keyboardType;
+  final bool? obscureText;
+  final bool? enabled;
+  final bool? readOnly;
+  final bool? autofocus;
+  final int? maxLines;
+  final int? maxLength;
+  final TextInputAction? textInputAction;
+  final Function(String)? onChanged;
+  final Function(String)? onSubmitted;
+  final Function()? onEditingComplete;
+  final Function()? onTap;
+  final String? Function(String?)? validator;
+  final TextInputFormatter? inputFormatter;
+  const SDTextInput({
+    super.key,
+    this.controller,
+    this.hintText,
+    this.labelText,
+    this.keyboardType,
+    this.obscureText,
+    this.enabled,
+    this.readOnly,
+    this.autofocus,
+    this.maxLines,
+    this.maxLength,
+    this.textInputAction,
+    this.onChanged,
+    this.onSubmitted,
+    this.onEditingComplete,
+    this.onTap,
+    this.validator,
+    this.inputFormatter,
+  });
 
   @override
   State<SDTextInput> createState() => _SDTextInputState();
@@ -11,6 +48,23 @@ class SDTextInput extends StatefulWidget {
 class _SDTextInputState extends State<SDTextInput> {
   @override
   Widget build(BuildContext context) {
-    return PlatformTextFormField(); // todo: add props
+    return PlatformTextFormField(
+      controller: widget.controller,
+      hintText: widget.hintText,
+      textInputAction: widget.textInputAction,
+      keyboardType: widget.keyboardType,
+      obscureText: widget.obscureText,
+      enabled: widget.enabled,
+      readOnly: widget.readOnly,
+      autofocus: widget.autofocus,
+      maxLines: widget.maxLines,
+      maxLength: widget.maxLength,
+      onChanged: widget.onChanged,
+      onEditingComplete: widget.onEditingComplete,
+      onTap: widget.onTap,
+      onFieldSubmitted: widget.onSubmitted,
+      validator: widget.validator,
+      inputFormatters: widget.inputFormatter != null ? [widget.inputFormatter!] : null,
+    );
   }
 }
