501c07426cdd8d2c3c7bdf6ee391fcb12158755d
Emir Bolat <emirybolat@hotmail.com>
2025-06-22 02:31:29 +0300

    Rename SDAleart class to SDAlert for consistency and clarity in widget naming.

--- speedev_ui/lib/src/widgets/alert/alert.dart
+++ speedev_ui/lib/src/widgets/alert/alert.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-class SDAleart {
+class SDAlert {
   static void show({
     required BuildContext context,
     Widget? title,
