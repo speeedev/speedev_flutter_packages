6416f373b5cfc0ab2abdce303570d29572b58a18
Emir Bolat <emirybolat@hotmail.com>
2025-06-22 21:24:32 +0300

    Refactor SDThemeProvider to simplify theme management
    - Removed the direct initialization of _themeData and replaced it with a late-initialized themeData property.
    - Introduced themeMode property to manage light and dark themes more effectively.
    - Updated setThemeMode method to reflect the new structure and ensure proper theme switching.

--- speedev_ui/lib/src/providers/theme_provider.dart
+++ speedev_ui/lib/src/providers/theme_provider.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
 class SDThemeProvider extends ChangeNotifier {
-  late ThemeData _themeData;
+  SDThemeProvider();
 
-  SDThemeProvider({required ThemeData themeData}) : _themeData = themeData;
-
-  ThemeData get themeData => _themeData;
-
+  ThemeMode themeMode = ThemeMode.light;
+  late ThemeData themeData;
   late ThemeData lightTheme;
   late ThemeData darkTheme;
 
   void setThemeMode(ThemeMode themeMode) {
-    _themeData = themeMode == ThemeMode.light ? lightTheme : darkTheme;
+    this.themeMode = themeMode;
+    themeData = themeMode == ThemeMode.light ? lightTheme : darkTheme;
     notifyListeners();
   }
 
