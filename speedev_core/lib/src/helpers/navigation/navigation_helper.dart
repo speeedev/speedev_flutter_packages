f61e4a74ed7f44b008d1589a7347dc2eec75f05d
Emir Bolat <emirybolat@hotmail.com>
2025-06-23 01:52:33 +0300

    Refactor SDNavHelper to utilize context for navigation methods
    - Updated navigation methods to use the context's router instead of a class-level router instance.
    - Simplified the navigation logic for better integration with the Flutter framework.

--- speedev_core/lib/src/helpers/navigation/navigation_helper.dart
+++ speedev_core/lib/src/helpers/navigation/navigation_helper.dart
@@ -2,40 +2,27 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class SDNavHelper {
-  late RootStackRouter router;
-
-  void init({
-    required RootStackRouter router,
-  }) {
-    this.router = router;
-  }
-
-  static SDNavHelper? _instance;
-  factory SDNavHelper() => _instance ??= SDNavHelper._();
-
-  SDNavHelper._();
-
   Future<void> navigate(BuildContext context, {required PageRouteInfo route, Map<String, dynamic>? params}) async {
-    await router.push(route.copyWith(params: params));
+    await context.router.push(route.copyWith(params: params));
   }
 
   Future<void> pop(BuildContext context) async {
-    await router.maybePop();
+    await context.router.maybePop();
   }
 
   void popUntil(BuildContext context, {required RoutePredicate predicate}) {
-    router.popUntil(predicate);
+    context.router.popUntil(predicate);
   }
 
   void popUntilRoot(BuildContext context) async {
-    router.popUntilRoot();
+    context.router.popUntilRoot();
   }
 
   Future<void> replace(BuildContext context, {required PageRouteInfo route}) async {
-    await router.replace(route);
+    await context.router.replace(route);
   }
 
   Future<void> replaceAll(BuildContext context, {required List<PageRouteInfo> pages}) async {
-    await router.replaceAll(pages);
+    await context.router.replaceAll(pages);
   }
 }
