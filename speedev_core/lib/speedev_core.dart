21c3c77674b14e8907befe646b1c9496d96f8548
Emir Bolat <emirybolat@hotmail.com>
2025-07-04 20:09:09 +0300

    Update exports in speedev_core.dart to include navigation_logger_helper for enhanced navigation logging capabilities

--- speedev_core/lib/speedev_core.dart
+++ speedev_core/lib/speedev_core.dart
@@ -17,3 +17,4 @@ export 'src/helpers/speedev_core_functions.dart';
 export 'src/helpers/logger/logger_helper.dart';
 export 'src/services/firebase/storage/storage_service.dart';
 export 'src/helpers/storage/local_storage_helper.dart';
+export 'src/helpers/navigation/navigation_logger_helper.dart';
