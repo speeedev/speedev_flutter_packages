c1cd13d82daaf8b04ed2915cc0dc0ce637284952
Emir Bolat <emirybolat@hotmail.com>
2025-07-23 22:25:15 +0300

    Enhance SDText widget to use context-based text style
    - Updated the SDText widget to default to the context's bodyMedium text style when no style is provided, improving consistency with the overall theme and enhancing text rendering.

--- speedev_ui/lib/src/widgets/text/text.dart
+++ speedev_ui/lib/src/widgets/text/text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:speedev_ui/speedev_ui.dart';
 
 class SDText extends StatelessWidget {
   final String text;
@@ -19,6 +20,7 @@ class SDText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformText(text, style: style, textAlign: textAlign, overflow: overflow, maxLines: maxLines);
+    final textStyle = style ?? context.textTheme.bodyMedium;
+    return PlatformText(text, style: textStyle, textAlign: textAlign, overflow: overflow, maxLines: maxLines);
   }
 }
