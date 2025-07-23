24726cf139b1abf23d3c880eea8cf92bbeea7578
Emir Bolat <emirybolat@hotmail.com>
2025-07-23 22:39:25 +0300

    Remove unused context extension import from SDListTile for cleaner code

--- speedev_ui/lib/src/widgets/list_tile/list_tile.dart
+++ speedev_ui/lib/src/widgets/list_tile/list_tile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:speedev_ui/speedev_ui.dart';
-import 'package:speedev_ui/src/helpers/extensions/context_extension.dart';
 
 class SDListTile extends StatelessWidget {
   final Widget title;
