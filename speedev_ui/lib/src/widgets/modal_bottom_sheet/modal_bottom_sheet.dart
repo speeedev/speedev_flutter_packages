2e460559582a52a5416dcf6a425c2f3c542017d0
Emir Bolat <emirybolat@hotmail.com>
2025-08-31 01:02:09 +0300

    Enhance SDModalBottomSheet with customizable top padding and dismissible option
    - Added topPadding parameter to control the space above the modal content.
    - Introduced isDismissible parameter to manage modal dismissal behavior.
    - Updated modal display logic to include padding and improved container styling for better visual presentation.

--- speedev_ui/lib/src/widgets/modal_bottom_sheet/modal_bottom_sheet.dart
+++ speedev_ui/lib/src/widgets/modal_bottom_sheet/modal_bottom_sheet.dart
@@ -5,11 +5,29 @@ class SDModalBottomSheet {
   static void show({
     required BuildContext context,
     required Widget child,
+    double topPadding = 80.0, // Üstten ne kadar boşluk bırakılacağı
+    bool isDismissible = true,
   }) {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       showPlatformModalSheet(
         context: context,
-        builder: (context) => child,
+        builder: (context) => Padding(
+          padding: EdgeInsets.only(top: topPadding),
+          child: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+              ),
+            ),
+            child: child,
+          ),
+        ),
+        cupertino: CupertinoModalSheetData(anchorPoint: Offset(0, 0)),
+        material: MaterialModalSheetData(
+          isDismissible: isDismissible,
+          isScrollControlled: true,
+        ),
       );
     });
   }
